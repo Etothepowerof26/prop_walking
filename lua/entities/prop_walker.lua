57b51421598ea2cc40ea37fb7de55c0f1048505a
etothepowerof26 <jdhale2004@gmail.com>
2019-03-28 12:27:02 -0400

    Change from using timers to using the ent's Think hook
    ... and also move some things around

--- lua/entities/prop_walker.lua
+++ lua/entities/prop_walker.lua
@@ -1,44 +1,33 @@
 AddCSLuaFile()
-
 DEFINE_BASECLASS("base_anim")
 
 local InactiveTimerDelay = GetConVar("propwalker_remove_delay"):GetInt() * 60
 
 --[[-------------------------------------------------------------------------
 Removal the prop walker after 2 minutes of inactivity
-
-(TODO: change the remove timer to it's think hook?)
 ---------------------------------------------------------------------------]]
 function ENT:RestartRemovalTimer()
+	self.__spawnat = SysTime()
+end
 
-	return timer.Start("pwRemove_".. self:EntIndex())
-
+function ENT:Think()
+	if SysTime() > self.__spawnat + InactiveTimerDelay then
+		SafeRemoveEntity(self)
+	end
 end
 
 -- A global table to find all prop walkers without iterating all entities
 propWalkers = propWalkers or {}
 
-local function HandleRemoval(pw)
-
-	timer.Remove("pwRemove_".. pw:EntIndex())
-
-	table.remove(propWalkers, pw.PWIndex)
-
-end
-
-
 --[[-------------------------------------------------------------------------
 Init the prop walker and cover the ground
 ---------------------------------------------------------------------------]]
 function ENT:Initialize()
-
 	local ground = self:GetNW2Entity("Ground")
-	if SERVER and !IsValid(ground) then
-
+	if SERVER and not IsValid(ground) then
 		self:Remove()
 
 		return
-
 	end
 
 	-- Cover the new ground
@@ -55,7 +44,9 @@ function ENT:Initialize()
 	-- Add the new prop walker to the table of prop walkers for quicker filtering in collision traces and store its index for removal
 	self.PWIndex = table.insert(propWalkers, self)
 	-- And remove ourselves from the table upon removal
-	self:CallOnRemove("HandleRemoval", HandleRemoval)
+	self:CallOnRemove("HandleRemoval", function(ent)
+		table.remove(propWalkers, ent.PWIndex)
+	end)
 	-- Make the prop walker invisible
 	self:DrawShadow(false)
 	self:SetNoDraw(true)
@@ -64,22 +55,14 @@ function ENT:Initialize()
 	self:SetCollisionGroup(COLLISION_GROUP_NONE)
 	self:EnableCustomCollisions(true)
 	
+	self.__spawnat = SysTime()
+	
 	if SERVER then
 		-- Network our abs position as soon as possible for smooth player mounts
 		self:NetworkAbsPosition()
-
-		-- Create a self removal timer in case of inactivity
-		timer.Create("pwRemove_".. self:EntIndex(), InactiveTimerDelay, 1, function()
-
-			SafeRemoveEntity(self)
-
-		end)
-		-- We want to use touch hooks to determine ground collision groups
-		-- self:SetTrigger(true)
 	end
-	
-end
 
+end
 
 --[[-------------------------------------------------------------------------
 Rebuild the physics of the prop walker to match our ground's model
@@ -95,9 +78,7 @@ function ENT:RebuildPhysics(model)
 	self:SetSolid(SOLID_VPHYSICS)
 	local phys = self:GetPhysicsObject()
 	if IsValid(phys) then
-
 		phys:EnableMotion(false)
-
 	end
 
 	self:EnableCustomCollisions(true)
@@ -110,27 +91,23 @@ end
  Handles collisions against traces, includes player movement
 ---------------------------------------------------------------------------]]
 function ENT:TestCollision(startpos, delta, isbox, extents, mask)
-
-	if !self.PhysCollides then return end
+	if not self.PhysCollides then return end
 	-- Return nothing if our mask isn't involved with actual player collisions
 	if bit.band(mask, CONTENTS_PLAYERCLIP) == 0 then return end
 	-- TraceBox expects the trace to begin at the center of the box, but TestCollision is very quite silly
-	local max = extents
-	local min = -extents
+	local max, min = extents, -extents
 	max.z = max.z - min.z
 	min.z = 0
 
 	for k, v in ipairs(self.PhysCollides) do
-
 		local hit, norm, frac = v:TraceBox(self:GetPos(), self:GetAngles(), startpos, startpos + delta, min, max)
-		if !hit then continue end
+		if not hit then continue end
 
 		return {
 			HitPos = hit,
 			Normal = norm,
 			Fraction = frac,
 		}
-
 	end
 end
 
@@ -142,7 +119,7 @@ function ENT:NetworkAbsPosition(pos, ang)
 	if CLIENT then return pos, ang end
 
 	local ground = self:GetNW2Entity("Ground")
-	if !IsValid(ground) then return end
+	if not IsValid(ground) then return end
 
 	local currPos = self:GetNW2Vector("GroundPos")
 	local currAng = self:GetNW2Vector("GroundAng")
@@ -150,11 +127,11 @@ function ENT:NetworkAbsPosition(pos, ang)
 	local newPos = ground:GetPos()
 	local newAng = ground:GetAngles()
 	-- Only network if there was a change
-	if !currPos or currPos != newPos then
+	if not currPos or currPos ~= newPos then
 		self:SetNW2Vector("GroundPos", newPos)
 	end
 
-	if !currAng or currAng != newAng then
+	if not currAng or currAng ~= newAng then
 		self:SetNW2Angle("GroundAng", newAng)
 	end
 end
